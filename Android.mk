3dabe67b2a30c0a6241fee3be0465abd053d5f45
Kan-Ru Chen <kanru@0xlab.org>
2010-07-08 15:04:27 +0800

    [TSCalibration2] Remove development and eng tag from LOCAL_MODULE_TAGS
    Change-Id: I9a88b010b31cd65f03495dac1cf1f580456c2c2b

--- Android.mk
+++ Android.mk
@@ -16,7 +16,7 @@
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
-LOCAL_MODULE_TAGS := user development eng
+LOCAL_MODULE_TAGS := user
 LOCAL_SRC_FILES := $(call all-subdir-java-files)
 LOCAL_PACKAGE_NAME := TSCalibration2
 LOCAL_OVERRIDES_PACKAGES := TSCalibration
